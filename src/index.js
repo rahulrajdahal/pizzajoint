81c690aa121ef7be36540912e85f3752acdeac79
Markaa10 <rahulrajdahal@gmail.com>
2021-10-09 20:37:17 +0545

    animationlearned

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
